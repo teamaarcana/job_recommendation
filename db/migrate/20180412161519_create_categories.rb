ba9cb17a8914796678850adaf6cd377f567071c3
ran010 <ranjanbajra@gmail.com>
2018-04-12 22:57:36 +0545

    Set up carriewave

---
+++ db/migrate/20180412161519_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :categories do |t|
+      t.string :category_name
+      t.integer :seeker_id
+
+      t.timestamps
+    end
+    add_index :categories, :seeker_id
+
+  end
+end

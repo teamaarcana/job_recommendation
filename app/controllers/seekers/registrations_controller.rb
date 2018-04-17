d42c6f86d421f8736a9153e632e9447b2a303632
ran010 <ranjanbajra@gmail.com>
2018-04-17 12:34:08 +0545

    edit page redirect and working of edit of seeker

--- app/controllers/seekers/registrations_controller.rb
+++ app/controllers/seekers/registrations_controller.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-class Recuitors::RegistrationsController < Devise::RegistrationsController
+class Seekers::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
   # def new
@@ -39,24 +38,20 @@ class Recuitors::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-   #The path used after sign up.
   def after_sign_up_path_for(resource)
-     edit_recuitor_registration_path
+     edit_seeker_registration_path
   end
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit :account_update, keys: [:logo, :description]
+        devise_parameter_sanitizer.permit :account_update, keys: [:location, :photo,:prefered_loc,
+                                                                :experience,:salary,:dob,:nationality,:perm_address,
+                                                                :temp_address,:cv,:job_status,:description,:certificate]
   end
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :recuitors, path: 'recuitors',controllers: {registrations: 'recuitors/registrations'}
   # eg. http://localhost:3000/users/sign_in
-  devise_for :seekers, path: 'seekers'
+  devise_for :seekers, path: 'seekers',controllers: {registrations: 'seekers/registrations'}
   # eg. http://localhost:3000/admins/sign_in
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
